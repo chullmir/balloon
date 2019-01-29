72c69f81f7cc65c6c1b3d8cc3c2e1bd5a6212b3c
chullmir <paulochullmir@gmail.com>
2019-01-29 09:20:31 -0300

    validacion js en register

--- public/js/validateRegister.js
+++ public/js/validateRegister.js
@@ -1,25 +1,49 @@
-var nombre = document.getElementById('nombre');
-var blankName = document.getElementById('blankName');
-nombre.onblur = function(){
-	if (this.value == "") {
-		blankName.setAttribute('class','error');
-	} else {
-		blankName.setAttribute('class','d-none');
-	}
-};
-
-var lastname = document.getElementById('lastname');
-var blankLastName = document.getElementById('blankLastName');
-lastname.onblur = function(){
-	if (this.value == "") {
-		blankLastName.setAttribute('class','error');
-	} else {
-		blankLastName.setAttribute('class','d-none');
-	}
-}
-
-fetch("https://restcountries.eu/rest/v2/all"){
-	.then(function (response){
-		return response.json();
+var theForm = document.getElementById('registerForm');
+var elementsOfForm = theForm.elements;
+var elementsInArray = Array.from(elementsOfForm);
+elementsInArray.pop();
+elementsInArray.forEach(function(element){
+	element.addEventListener("blur",function(){
+		if (this.value == "") {
+			this.classList.add("is-invalid");
+		}
 	})
-}
+});
+// var inputName = document.querySelector("input[name='nombre']");
+// var inputLastName = document.querySelector("input[name='lastname']");
+// var inputCountry = document.querySelector("input[name='country']");
+// var inputEmail = document.querySelector("input[name='email']");
+// var inputPassword = document.querySelector("input[name='password']");
+// theForm.onsubmit = function(event){
+// 	if (inputName.value = "") {
+// 		event.preventDefault();
+// 		inputName.classList.add('error');
+
+// 	}
+// };
+
+// var nombre = document.getElementById('nombre');
+// var blankName = document.getElementById('blankName');
+// nombre.onblur = function(){
+// 	if (this.value == "") {
+// 		blankName.setAttribute('class','error');
+// 	} else {
+// 		blankName.setAttribute('class','d-none');
+// 	}
+// };
+
+// var lastname = document.getElementById('lastname');
+// var blankLastName = document.getElementById('blankLastName');
+// lastname.onblur = function(){
+// 	if (this.value == "") {
+// 		blankLastName.setAttribute('class','error');
+// 	} else {
+// 		blankLastName.setAttribute('class','d-none');
+// 	}
+// }
+
+// // fetch("https://restcountries.eu/rest/v2/all"){
+// // 	.then(function (response){
+// // 		return response.json();
+// // 	})
+// // }
--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -7,50 +7,78 @@
  			<h4>Registrarse</h4>
  		</div>
  		<div class="col-12">
- 			<form class="form-group" method="post" enctype="multipart/form-data" action="{{route('register')}}" id="registerForm">
+ 			<form class="form-group" method="post" enctype="multipart/form-data" action="{{route('register')}}">
  				@csrf
  				<div class="form-group">
  					
  					<input type="text" name="nombre" class="form-control form-name {{ $errors->has('nombre') ? ' is-invalid' : '' }}" placeholder="Nombre" id="nombre" value="{{old('nombre')}}">
  				</div>
- 				@if($errors->has('nombre'))
- 					<div class="error">
- 						<strong> {{$errors->first('nombre')}} </strong>
- 					</div>
-
-
- 				@endif
+ 				<div class="error {{$errors->has('nombre') ? '' : 'd-none'}}" id="blankName">
+ 					Por favor completar el nombre
+ 				</div>
  				
 
 				<div class="form-group">
 					<input type="text" name="lastname" class="form-control form-lastname {{ $errors->has('lastname') ? ' is-invalid' : '' }}" placeholder="Apellido*" id="lastname" value="{{old('lastname')}}">
 				</div>
+				<div class="error {{$errors->has('lastname') ? '' : 'd-none'}}" id="blankLastName">
+ 					Por favor completar el apellido
+ 				</div>
 				
+				{{-- @if ($errors->has('lastname'))
+ 					<div class="alert alert-danger">
+ 						<strong>{{ $errors->first('lastname') }}</strong>
+ 					</div>
+				@endif --}}
 
 				<div class="form-group">
 					<input type="text" name="country" class="form-control {{ $errors->has('lastname') ? ' is-invalid' : '' }}" placeholder="País*" value="{{old('country')}}">
 				</div>
+				@if ($errors->has('country'))
+ 					<div class="alert alert-danger">
+ 						<strong>{{ $errors->first('country') }}</strong>
+ 					</div>
+				@endif
+
+				{{-- <div class="form-group">
+					<label for="FormFileAvatar" class="mt-3">Subir una foto de perfil:</label>
+					<input type="file" class="form-control-file" id="FormFileAvatar" name="avatar" id="avatar">
+				</div> --}}
 
  				
 
  				<div class="form-group">
  					<input type="text" name="email" class="form-control form-email {{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="E-mail*" id="email" value="{{old('email')}}">
  				</div>
+ 				@if ($errors->has('email'))
+ 					<div class="alert alert-danger">
+ 						<strong>{{ $errors->first('email') }}</strong>
+ 					</div>
+				@endif
  				
  				<div class="form-group">
  					<input type="password" name="password" class="form-control form-password {{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Constraseña*" id="password">
  				</div>
  				<div class="aclaracion">
  					<i class="fas fa-info-circle"></i>
- 					<small>La contraseña debe tener 6 caracteres como mínimo.</small>
+ 					<small>La contraseña debe tener 3 caracteres como mínimo.</small>
  				</div>
+ 				@if ($errors->has('password'))
+ 					<div class="alert alert-danger">
+ 						<strong>{{ $errors->first('password') }}</strong>
+ 					</div>
+				@endif
 
- 				
 
 				<div class="form-group">
 					<input type="password" name="password_confirmation" class="form-control form-password {{ $errors->has('password-confirm') ? ' is-invalid' : '' }}" placeholder="Confirmar contraseña*" id="password-confirm">
 				</div>
-				
+				@if ($errors->has('password-confirm'))
+ 					<div class="alert alert-danger">
+ 						<strong>{{ $errors->first('password-confirm') }}</strong>
+ 					</div>
+				@endif
+
  				
  				<!-- <label for="FormFileAvatar" class="mt-3">Subir una foto de perfil:</label>
  				<input type="file"
@@ -66,3 +94,4 @@
  	</div>
  	<script src="js/validateRegister.js"></script>
 </body>
+
